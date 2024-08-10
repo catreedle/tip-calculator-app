22ee8dd1fd0783cf74851d893899dde4778b649e
Purnama S Rahayu <purnamasrahayu@gmail.com>
2024-08-10 08:41:52 +0700

    custom input tip

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 function calculateTip(bill, tipPercentage, numberOfPeople) {
-  const tipAmount = ((bill * (tipPercentage / 100)) / 5).toFixed(2);
+  const tipAmount = ((bill * (tipPercentage / 100)) / numberOfPeople).toFixed(
+    2
+  );
 
   const totalAmount = (bill / numberOfPeople + parseFloat(tipAmount)).toFixed(
     2
@@ -30,6 +32,13 @@ function resetCalculator() {}
 const billInputElement = document.getElementById("bill");
 const peopleInputElement = document.getElementById("number-of-people");
 const selectTipElement = document.getElementsByName("select-tip");
+const selectTipCustomRadioElement = document.querySelector(
+  ".calculator-tip__radio__custom"
+);
+const selectTipCustomContainerElement = document.querySelector(
+  ".calculator-tip__select__custom__container"
+);
+const selectTipCustomInputElement = document.getElementById("custom-input");
 
 let bill = "";
 let numberOfPeople = "";
@@ -37,21 +46,21 @@ let tipPercentage = "";
 
 billInputElement.addEventListener("input", function (e) {
   bill = handleInputChange(e);
-  billErrorElement = document.querySelector(
+  const billErrorElement = document.querySelector(
     ".calculator-input__bill-error"
   );
-
 
   if (!validateInput(bill)) {
     billErrorElement.classList.remove("hidden");
     billErrorElement.classList.add("visible");
-    billInputElement.classList.add("calculator-input--error")
+    billInputElement.classList.add("calculator-input--error");
+    emptyCalulationResult();
 
     return;
   } else {
     billErrorElement.classList.remove("visible");
     billErrorElement.classList.add("hidden");
-    billInputElement.classList.remove("calculator-input--error")
+    billInputElement.classList.remove("calculator-input--error");
   }
   if (validateAllInputs(bill, tipPercentage, numberOfPeople)) {
     const result = calculateTip(bill, tipPercentage, numberOfPeople);
@@ -61,6 +70,9 @@ billInputElement.addEventListener("input", function (e) {
 });
 
 peopleInputElement.addEventListener("input", function (e) {
+  let value = e.target.value;
+  value = value.split(".")[0]; // prevent decimal input
+  e.target.value = value;
   numberOfPeople = handleInputChange(e);
 
   const peopleErrorElement = document.querySelector(
@@ -70,13 +82,14 @@ peopleInputElement.addEventListener("input", function (e) {
   if (!validateInput(numberOfPeople)) {
     peopleErrorElement.classList.remove("hidden");
     peopleErrorElement.classList.add("visible");
-    peopleInputElement.classList.add("calculator-input--error")
+    peopleInputElement.classList.add("calculator-input--error");
+    emptyCalulationResult();
 
     return;
   } else {
     peopleErrorElement.classList.remove("visible");
     peopleErrorElement.classList.add("hidden");
-    peopleErrorElement.classList.remove("calculator-input--error")
+    peopleErrorElement.classList.remove("calculator-input--error");
   }
   if (validateAllInputs(bill, tipPercentage, numberOfPeople)) {
     const result = calculateTip(bill, tipPercentage, numberOfPeople);
@@ -89,15 +102,54 @@ function handleInputChange(event) {
   return parseFloat(event.target.value);
 }
 
+function toggleCustomInputTip() {
+  selectTipCustomRadioElement.classList.add("hidden");
+  selectTipCustomRadioElement.classList.remove("visible");
+
+  selectTipCustomContainerElement.classList.toggle("hidden");
+  selectTipCustomContainerElement.classList.toggle("visible");
+}
+
+const tipErrorElement = document.querySelector(".calculator-input__tip-error");
+
 function handleRadioChange(event) {
-  const selectedValue = parseFloat(event.target.value);
+  let value = event.target.value;
+  if (value == "custom") {
+    emptyCalulationResult();
+    toggleCustomInputTip();
+    selectTipCustomInputElement.addEventListener("input", function (e) {
+      value = e.target.value;
+      tipPercentage = parseFloat(value);
 
-  tipPercentage = selectedValue;
+      if (!validateInput(value)) {
+        emptyCalulationResult();
+        tipErrorElement.classList.remove("hidden");
+        tipErrorElement.classList.add("visible");
+      }
 
-  if (validateAllInputs(bill, tipPercentage, numberOfPeople)) {
-    const result = calculateTip(bill, tipPercentage, numberOfPeople);
+      if (validateAllInputs(bill, tipPercentage, numberOfPeople)) {
+        const result = calculateTip(bill, tipPercentage, numberOfPeople);
+        tipErrorElement.classList.add("hidden");
+        tipErrorElement.classList.remove("visible");
+        showCalculationResult(result);
+      }
+    });
+  } else {
+    selectTipCustomRadioElement.classList.remove("hidden");
+    selectTipCustomRadioElement.classList.add("visible");
+    selectTipCustomContainerElement.classList.remove("visible");
+    selectTipCustomContainerElement.classList.add("hidden");
+    tipErrorElement.classList.add("hidden");
+    tipErrorElement.classList.remove("visible");
 
-    showCalculationResult(result);
+    const selectedValue = parseFloat(value);
+    tipPercentage = selectedValue;
+
+    if (validateAllInputs(bill, tipPercentage, numberOfPeople)) {
+      const result = calculateTip(bill, tipPercentage, numberOfPeople);
+
+      showCalculationResult(result);
+    }
   }
 }
 
@@ -105,14 +157,21 @@ selectTipElement.forEach((option) => {
   option.addEventListener("change", handleRadioChange);
 });
 
+const tipAmountElement = document.querySelector(
+  ".calculator-result__tip__amount"
+);
+
+const totalAmountElement = document.querySelector(
+  ".calculator-result__total__amount"
+);
+
 function showCalculationResult(result) {
-  const tipAmountElement = document.querySelector(
-    ".calculator-result__tip__amount"
-  );
   tipAmountElement.textContent = "$" + result.tipAmount;
 
-  const totalAmountElement = document.querySelector(
-    ".calculator-result__total__amount"
-  );
   totalAmountElement.textContent = "$" + result.totalAmount;
+}
+
+function emptyCalulationResult() {
+  tipAmountElement.textContent = "";
+  totalAmountElement.textContent = "";
 }
